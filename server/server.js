ea10f072ccd121116af1fe1064104284559ecc6c
AliakbarSu <aliakbar.su@hotmail.com>
2021-09-15 11:18:44 +1200

    Setup graphql

--- server/server.js
+++ server/server.js
@@ -1,12 +1,46 @@
 const path = require('path')
 const express = require('express')
 const server = express()
+const { graphqlHTTP } = require('express-graphql')
+const { buildSchema } = require('graphql')
 
 server.use(express.json())
 server.use(express.static(path.join(__dirname, 'public')))
 
-server.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, './public/index.html'))
-})
+const schema = buildSchema(`
+
+type User {
+  id: Int
+  name: String
+  email: String
+  favourite_animal: String
+  favourite_programming_language: String
+}
+
+  type RootQuery {
+    user: User
+  }
+
+  schema {
+    query: RootQuery
+  }
+`)
+
+server.use(
+  '/graphql',
+  graphqlHTTP({
+    schema: schema,
+    rootValue: {
+      user: {
+        id: 1,
+        name: 'Ali',
+        email: 'unknown@example.com',
+        favourite_animal: 'cat',
+        favourite_programming_language: 'node.js'
+      }
+    },
+    graphiql: true
+  })
+)
 
 module.exports = server
